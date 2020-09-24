36eadbdc36a0fb64c69e8ed4a91c3751116aa734
AlenS <alenskupnjak@yahoo.com>
2020-09-24 16:13:04 +0200

    015 card dodan

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -12,6 +12,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   imports: [
@@ -28,6 +29,7 @@ import { MatTabsModule } from '@angular/material/tabs';
     MatToolbarModule,
     MatListModule,
     MatTabsModule,
+    MatCardModule,
   ],
   exports: [
     MatSliderModule,
@@ -43,6 +45,7 @@ import { MatTabsModule } from '@angular/material/tabs';
     MatToolbarModule,
     MatListModule,
     MatTabsModule,
+    MatCardModule,
   ],
 })
 export class MaterialModule {}
